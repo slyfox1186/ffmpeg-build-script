c3ded2d2e6a13210bd0552c1373ea1d78ecd90b7
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-12 11:33:44 -0400

    Update install-free.sh

--- install-free.sh
+++ install-free.sh
@@ -6,17 +6,15 @@ clear
 
 # VERIFY THE SCRIPT DOES NOT HAVE ROOT ACCESS BEFORE CONTINUING
 # THIS CAN CAUSE ISSUES USING THE 'IF WHICH' COMMANDS IF RUN AS ROOT
-if [ "${EUID}" -lt '1' ]; then
-    echo 'You must run this script WITHOUT root/sudo'
-    echo
-    exit 1
+if [ "$EUID" -eq '0' ]; then
+    exec bash "$0" "$@"
 fi
 
 make_dir ()
 {
-    if [ ! -d "${1}" ]; then
-        if ! mkdir "${1}"; then            
-            printf '\n Failed to create dir %s' "${1}";
+    if [ ! -d "$1" ]; then
+        if ! mkdir "$1"; then            
+            printf '\n%s\n\n' "Failed to create dir: $1";
             exit 1
         fi
     fi    
@@ -24,33 +22,33 @@ make_dir ()
 
 command_exists()
 {
-    if ! [[ -x $(command -v "${1}") ]]; then
+    if ! [[ -x $(command -v "$1") ]]; then
         return 1
     fi
 
     return 0
 }
 
-BUILD_DIR='ffmpeg-build'
+build_dir='ffmpeg-build'
 
 if ! command_exists 'curl'; then
-    echo 'curl command not installed.'
-    echo
+    printf '\n%s\n\n%s\n\n' \
+        'The curl command is not installed.' \
+        'To install it run the command: sudo apt install curl'
     exit 1
 fi
 
 echo 'ffmpeg-build-script-downloader v0.1'
-echo '===================================='
-echo
-
-echo 'First we create the ffmpeg build directory' "${BUILD_DIR}"
 echo '========================================================'
 echo
-make_dir "${BUILD_DIR}"
-cd "${BUILD_DIR}" || exit 1
+
+echo 'First we create the ffmpeg build directory' "$build_dir"
+echo '========================================================'
+echo
+make_dir "$build_dir" && cd "$build_dir" || exit 1
 
 echo 'Now we download and execute the build script'
-echo '============================================'
+echo '========================================================'
 echo
 
-bash <(curl -s 'https://raw.githubusercontent.com/slyfox1186/ffmpeg-build-script/main/build-ffmpeg.sh') --build --latest
+bash <(curl -sSL 'https://raw.githubusercontent.com/slyfox1186/ffmpeg-build-script/main/build-ffmpeg.sh') --build --latest
