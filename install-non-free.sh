b602f11ddccc5867ff3c1dd20895b5b09e115c77
Jeff <37704019+slyfox1186@users.noreply.github.com>
2023-04-12 11:35:03 -0400

    Update install-non-free.sh

--- install-non-free.sh
+++ install-non-free.sh
@@ -6,17 +6,15 @@ clear
 
 # VERIFY THE SCRIPT DOES NOT HAVE ROOT ACCESS BEFORE CONTINUING
 # THIS CAN CAUSE ISSUES USING THE 'IF WHICH' COMMANDS IF RUN AS ROOT
-if [ "${EUID}" -eq '0' ]; then
-    echo 'You must run this script WITHOUT root/sudo'
-    echo
-    exec bash "${0}" "${@}"
+if [ "$EUID" -eq '0' ]; then
+    exec bash "$0" "$@"
 fi
 
-make_dir()
+make_dir ()
 {
-    if [ ! -d "${1}" ]; then
-        if ! mkdir "${1}"; then            
-            printf '\nFailed to create directory: %s' "${1}";
+    if [ ! -d "$1" ]; then
+        if ! mkdir "$1"; then            
+            printf '\n%s\n\n' "Failed to create dir: $1";
             exit 1
         fi
     fi    
@@ -24,35 +22,33 @@ make_dir()
 
 command_exists()
 {
-    if ! [[ -x $(command -v "${1}") ]]; then
+    if ! [[ -x $(command -v "$1") ]]; then
         return 1
     fi
 
     return 0
 }
 
-build_dir="${PWD}/ffmpeg-build"
+build_dir='ffmpeg-build'
 
 if ! command_exists 'curl'; then
-    echo 'curl command not installed.'
-    echo
+    printf '\n%s\n\n%s\n\n' \
+        'The curl command is not installed.' \
+        'To install it run the command: sudo apt install curl'
     exit 1
 fi
 
 echo 'ffmpeg-build-script-downloader v0.1'
-echo '============================================='
+echo '========================================================'
 echo
 
-echo 'First we create the ffmpeg build directory'
-echo '=============================================='
+echo 'First we create the ffmpeg build directory' "$build_dir"
+echo '========================================================'
 echo
-
-make_dir "${build_dir}"
-
-cd "${build_dir}" || exit 1
+make_dir "$build_dir" && cd "$build_dir" || exit 1
 
 echo 'Now we download and execute the build script'
-echo '=============================================='
+echo '========================================================'
 echo
 
 bash <(curl -sSL 'https://raw.githubusercontent.com/slyfox1186/ffmpeg-build-script/main/build-ffmpeg.sh') --build --enable-gpl-and-non-free --latest
