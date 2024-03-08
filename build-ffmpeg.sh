74a372f564d9c6487fc6107f5c0febc86df984de
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-08 12:30:41 -0500

    Update build-ffmpeg.sh
    Signed-off-by: Jeff <37704019+slyfox1186@users.noreply.github.com>

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -666,7 +666,7 @@ usage() {
     echo "    -n, --enable-gpl-and-non-free    Enable GPL and non-free codecs - https://ffmpeg.org/legal.html"
     echo "    -l, --latest                     Force the script to build the latest version of dependencies if newer version is available"
     echo
-    echo "Example: sudo bash $script_name --build -n --latest"
+    echo "Example: bash $script_name --build -n --latest"
     echo
 }
 
@@ -678,7 +678,7 @@ while (("$#" > 0)); do
     case "$1" in
         -h|--help) usage; exit 0 ;;
         -v|--version) echo; log "The script version is: $script_ver"; exit 0 ;;
-        -n|--enable-gpl-and-non-free) 
+        -n|--enable-gpl-and-non-free)
             CONFIGURE_OPTIONS+=("--enable-"{gpl,libsmbclient,libcdio,nonfree})
             NONFREE_AND_GPL=true
             ;;
@@ -1141,34 +1141,39 @@ apt_pkgs() {
     local libunwind_pkg=$(find_latest_pkg_version 'libunwind*' 'libunwind-[0-9]+-dev')
 
     # Define an array of apt package names
-    pkgs=($1 $libcppabi_pkg $libcpp_pkg $libunwind_pkg $openjdk_pkg ant apt asciidoc autoconf
-          autoconf-archive automake autopoint binutils bison build-essential cargo cargo-c
-          ccache checkinstall cmake curl doxygen fcitx-libs-dev flex flite1-dev freeglut3-dev
-          frei0r-plugins-dev gawk gettext gimp-data git gnome-desktop-testing gnustep-gui-runtime
-          google-perftools gperf gtk-doc-tools guile-3.0-dev help2man jq junit ladspa-sdk
-          lib32stdc++6 libamd2 libasound2-dev libass-dev libaudio-dev libavfilter-dev libbabl-0.1-0
-          libbluray-dev libbpf-dev libbs2b-dev libbz2-dev libc6 libc6-dev libcaca-dev libcairo2-dev
-          libcamd2 libccolamd2 libcdio-dev libcdio-paranoia-dev libcdparanoia-dev libcholmod3 libdav1d-dev
-          libchromaprint-dev libcjson-dev libcodec2-dev libcolamd2 libcrypto++-dev libcurl4-openssl-dev
-          libdbus-1-dev libde265-dev libdevil-dev libdmalloc-dev libdrm-dev libdvbpsi-dev libebml-dev
-          libegl1-mesa-dev libffi-dev libgbm-dev libgdbm-dev libgegl-0.4-0 libgegl-common libgimp2.0
-          libgl1-mesa-dev libgles2-mesa-dev libglib2.0-dev libgme-dev libgmock-dev libgnutls28-dev
-          libgnutls30 libgoogle-perftools-dev libgoogle-perftools4 libgsm1-dev libgtest-dev libgvc6
-          libibus-1.0-dev libiconv-hook-dev libintl-perl libjack-dev libjemalloc-dev  libjxl-dev
-          libladspa-ocaml-dev libleptonica-dev liblz-dev liblzma-dev liblzo2-dev libmathic-dev
-          libmatroska-dev libmbedtls-dev libmetis5 libmfx-dev libmodplug-dev libmp3lame-dev
-          libmusicbrainz5-dev libmysofa-dev libnuma-dev libopencore-amrnb-dev libopencore-amrwb-dev
-          libopencv-dev libopenmpt-dev libopus-dev libpango1.0-dev libperl-dev libpstoedit-dev
-          libpulse-dev librabbitmq-dev libraqm-dev libraw-dev librsvg2-dev librubberband-dev
-          librust-gstreamer-base-sys-dev libshine-dev libsmbclient-dev libsnappy-dev libsndfile1-dev
-          libsndio-dev libsoxr-dev libspeex-dev libsqlite3-dev libssh-dev libssl-dev libsuitesparseconfig5
-          libsystemd-dev libtalloc-dev libtheora-dev libticonv-dev libtool libtool-bin libtwolame-dev
-          libudev-dev libumfpack5 libv4l-dev libva-dev libvdpau-dev libvidstab-dev libvlccore-dev
-          libvo-amrwbenc-dev libvpx-dev libx11-dev libxcursor-dev libxext-dev libxfixes-dev libxi-dev
-          libxkbcommon-dev libxrandr-dev libxss-dev libxvidcore-dev libzimg-dev libzmq3-dev libzstd-dev
-          libzvbi-dev libzzip-dev llvm lshw lzma-dev m4 mesa-utils meson nasm ninja-build pandoc python3
-          python3-pip python3-venv ragel re2c scons texi2html texinfo tk-dev unzip valgrind wget xmlto
-          zlib1g-dev
+    pkgs=($1 $libcppabi_pkg $libcpp_pkg $libunwind_pkg $openjdk_pkg ant apt asciidoc
+          autoconf autoconf-archive automake autopoint binutils bison build-essential
+          cargo cargo-c ccache checkinstall cmake curl doxygen fcitx-libs-dev flex
+          flite1-dev freeglut3-dev frei0r-plugins-dev gawk gettext gimp-data git
+          gnome-desktop-testing gnustep-gui-runtime google-perftools gperf gtk-doc-tools
+          guile-3.0-dev help2man jq junit ladspa-sdk lib32stdc++6 libamd2 libasound2-dev
+          libass-dev libaudio-dev libavfilter-dev libbabl-0.1-0 libbluray-dev libbpf-dev
+          libbs2b-dev libbz2-dev libc6 libc6-dev libcaca-dev libcairo2-dev libcamd2
+          libccolamd2 libcdio-dev libcdio-paranoia-dev libcdparanoia-dev libcholmod3
+          libchromaprint-dev libcjson-dev libcodec2-dev libcolamd2 libcrypto++-dev
+          libcurl4-openssl-dev libdav1d-dev libdbus-1-dev libde265-dev libdevil-dev
+          libdmalloc-dev libdrm-dev libdvbpsi-dev libebml-dev libegl1-mesa-dev libffi-dev
+          libgbm-dev libgdbm-dev libgegl-0.4-0 libgegl-common libgimp2.0 libgl1-mesa-dev
+          libgles2-mesa-dev libglib2.0-dev libgme-dev libgmock-dev libgnutls28-dev
+          libgnutls30 libgoogle-perftools-dev libgoogle-perftools4 libgsm1-dev libgtest-dev
+          libgvc6 libibus-1.0-dev libiconv-hook-dev libintl-perl libjack-dev libjemalloc-dev
+          libjxl-dev libladspa-ocaml-dev libldap2-dev libleptonica-dev liblilv-dev liblz-dev
+          liblzma-dev liblzo2-dev libmathic-dev libmatroska-dev libmbedtls-dev libmetis5
+          libmfx-dev libmodplug-dev libmp3lame-dev libmusicbrainz5-dev libmysofa-dev
+          libnuma-dev libopencore-amrnb-dev libopencore-amrwb-dev libopencv-dev libopenmpt-dev
+          libopus-dev libpango1.0-dev libperl-dev libplacebo-dev libpocketsphinx-dev
+          libpsl-dev libpstoedit-dev libpulse-dev librabbitmq-dev libraqm-dev libraw-dev
+          librsvg2-dev librtmp-dev librubberband-dev librust-gstreamer-base-sys-dev libserd-dev
+          libshine-dev libsmbclient-dev libsnappy-dev libsndfile1-dev libsndio-dev libsord-dev
+          libsoxr-dev libspeex-dev libsphinxbase-dev libsqlite3-dev libsratom-dev libssh-dev
+          libssl-dev libsuitesparseconfig5 libsystemd-dev libtalloc-dev libtheora-dev
+          libticonv-dev libtool libtool-bin libtwolame-dev libudev-dev libumfpack5 libv4l-dev
+          libva-dev libvdpau-dev libvidstab-dev libvlccore-dev libvo-amrwbenc-dev libvpx-dev
+          libx11-dev libxcursor-dev libxext-dev libxfixes-dev libxi-dev libxkbcommon-dev
+          libxrandr-dev libxss-dev libxvidcore-dev libzimg-dev libzmq3-dev libzstd-dev
+          libzvbi-dev libzzip-dev llvm lshw lzma-dev m4 mesa-utils meson nasm ninja-build
+          pandoc python3 python3-pip python3-venv ragel re2c scons texi2html texinfo tk-dev
+          unzip valgrind wget xmlto zlib1g-dev
 )
 
     # Initialize arrays for missing, available, and unavailable packages
@@ -1349,13 +1354,13 @@ arch_os_ver() {
 
         if ! pgrep -x pacman >/dev/null; then
             echo "Pacman is not running. Removing stale lock file..."
-            sudo rm /var/lib/pacman/db.lck
+            rm /var/lib/pacman/db.lck
         fi
     fi
 
     for pkg in "${arch_pkgs[@]}"; do
         echo "Installing $pkg..."
-        sudo pacman -Sq --needed --noconfirm $pkg 2>&1
+        pacman -Sq --needed --noconfirm $pkg 2>&1
     done
 
     # Set the path for the Python virtual environment
@@ -1494,9 +1499,11 @@ set_java_variables
 case "$OS" in
     Arch) iscuda=$(find /opt/cuda* -type f -name nvcc 2>/dev/null)
           cuda_path=$(find /opt/cuda* -type f -name nvcc 2>/dev/null | grep -Eo '^.*/bin?')
+          cuda_include_path=$(find /opt/cuda* -type f -name nvcc 2>/dev/null | sort -rV | head -n1 | grep -Eo '^.*/bin?' | sed 's/bin/include/')
           ;;
-    *)    iscuda=$(find /usr/local/cuda* -type f -name nvcc 2>/dev/null)
-          cuda_path=$(find /usr/local/cuda* -type f -name nvcc 2>/dev/null | grep -Eo '^.*/bin?')
+    *)    iscuda=$(find /usr/local/cuda* -type f -name nvcc 2>/dev/null | sort -rV | head -n1)
+          cuda_path=$(find /usr/local/cuda* -type f -name nvcc 2>/dev/null | sort -rV | head -n1 | grep -Eo '^.*/bin?')
+          cuda_include_path=$(find /usr/local/cuda* -type f -name nvcc 2>/dev/null | sort -rV | head -n1 | grep -Eo '^.*/bin?' | sed 's/bin/include/')
           ;;
 esac
 
@@ -2892,6 +2899,9 @@ if $NONFREE_AND_GPL; then
             CONFIGURE_OPTIONS+=("--enable-libnpp")
         fi
 
+        PATH+=":$cuda_path"
+        export PATH
+
         # Get the Nvidia GPU architecture to build CUDA
         # https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards
         nvidia_architecture
@@ -2967,21 +2977,6 @@ if build "vapoursynth" "R65"; then
 fi
 CONFIGURE_OPTIONS+=("--enable-vapoursynth")
 
-if $NONFREE_AND_GPL; then
-    find_git_repo "cyanreg/cyanrip" "1" "T"
-    if build "cyanrip" "$repo_version"; then
-        download "https://github.com/cyanreg/cyanrip/archive/refs/tags/v$repo_version.tar.gz" "cyanrip-$repo_version.tar.gz"
-        execute meson setup build --prefix="$workspace" \
-                                  --buildtype=release \
-                                  --default-library=static \
-                                  --strip
-        execute ninja "-j$cpu_threads" -C build
-        execute ninja -C build install
-        execute cp -f "$workspace/bin/cyanrip" /usr/local/
-        build_done "cyanrip" "$repo_version"
-    fi
-fi
-
 git_caller "https://chromium.googlesource.com/codecs/libgav1" "libgav1-git"
 if build "$repo_name" "${version//\$ /}"; then
     echo "Cloning \"$repo_name\" saving version \"$version\""
@@ -3043,9 +3038,9 @@ if build "libheif" "$repo_version"; then
     CFLAGS="-g -O3 -fno-lto -pipe -march=native"
     CXXFLAGS="-g -O3 -fno-lto -pipe -march=native"
     export CFLAGS CXXFLAGS
-    libde265_libs$(sudo find /usr/ -type f -name 'libde265.s*')
-    if [[ -f "$libde265_libs" ]] && [[ ! -f "/usr/lib/x86_64-linux-gnu/libde265.so" ]]; then
-        sudo ln -sf "$libde265_libs" "/usr/lib/x86_64-linux-gnu/libde265.so"
+    libde265_libs$(find /usr/ -type f -name 'libde265.s*')
+    if [[ -f "$libde265_libs" ]] && [[ ! -e "/usr/lib/x86_64-linux-gnu/libde265.so" ]]; then
+        ln -sf "$libde265_libs" "/usr/lib/x86_64-linux-gnu/libde265.so"
         chmod 755 "/usr/lib/x86_64-linux-gnu/libde265.so"
     fi
 
