d83bf29c0ef7bcfeb98d257dadf82420a6187e7f
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-03-22 15:33:17 -0400

    Update build-ffmpeg.sh
    Signed-off-by: Jeff <37704019+slyfox1186@users.noreply.github.com>

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2068,SC2162,SC2317 source=/dev/null
 
-## GitHub: https://github.com/slyfox1186/ffmpeg-build-script
-## Script version: 3.5.3
-## Updated: 03.16.24
-## Purpose: build ffmpeg from source code with addon development libraries
-##          also compiled from source to help ensure the latest functionality
-## Supported Distros: Arch Linux
-##                    Debian 11|12
-##                    Ubuntu (20|22|23).04 & 23.10
-## Supported architecture: x86_64
-## CUDA SDK Toolkit: Updated to version 12.4.0
-## Fixed: Removed a hidden binary symbol in the script that should not have been there
+# GitHub: https://github.com/slyfox1186/ffmpeg-build-script
+# Script version: 3.5.3
+# Updated: 03.16.24
+# Purpose: build ffmpeg from source code with addon development libraries
+#          also compiled from source to help ensure the latest functionality
+# Supported Distros: Arch Linux
+#                    Debian 11|12
+#                    Ubuntu (20|22|23).04 & 23.10
+# Supported architecture: x86_64
+# CUDA SDK Toolkit: Updated to version 12.4.0
+# Fixed: Removed a hidden binary symbol in the script that should not have been there
 
 if [[ "$EUID" -ne 0 ]]; then
     echo "You must run this script with root or sudo."
