ab87ab86e717a0ee44b563a62eed2631f0724de1
Jeff <37704019+slyfox1186@users.noreply.github.com>
2024-05-27 08:56:24 -0400

    Update build-ffmpeg.sh
    Signed-off-by: Jeff <37704019+slyfox1186@users.noreply.github.com>

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -496,7 +496,7 @@ library_exists() {
 
 determine_libtool_version() {
     case "$STATIC_VER" in
-        20.04|22.04|23.04|23.10)
+        20.04|22.04|23.04|23.10|msft)
             libtool_version="2.4.6"
             ;;
         11|12|24.04)
